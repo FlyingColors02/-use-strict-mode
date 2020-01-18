484f5b68617c0bd4ec74fe1af5a204aeaf2986df
FlyingColors02 <shraduborse97@gmail.com>
2020-01-18 22:00:52 +0530

    #shraddha borse: added use strict mode

---
+++ main.js
@@ -0,0 +1,38 @@
+"use strict"
+
+/*defining variable with 'keyword'*/
+let numValue=10; 
+console.log(numValue);//10
+
+
+/*defining variable without 'keyword'*/
+
+//BEFORE WRITING "use strict" MODEs
+
+/*
+numValue1=2;
+console.log(numValue1);//2
+
+strValue1="javascript";
+console.log(strValue1);//javascript
+
+boolValue1=true;
+console.log(boolValue1);//true         */
+
+//gives output even if not using any type of keyword
+// irrespective of data type
+
+
+//AFTER WRITING "use strict" MODE
+
+/*
+numValue2=2;
+console.log(numValue2);//numValue1 is not defined 
+
+strValue2="javascript";
+console.log(strValue2);//strValue2 is not defined   
+
+boolValue2=true;
+console.log(boolValue2);//boolValue2 is not defined    */
+
+//it makes javascript to follow strict guideline of syntax
